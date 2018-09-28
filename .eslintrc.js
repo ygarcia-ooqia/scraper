a0973a8e83bf5277df8a75164fbf144a013dc7dd
ygarcia <yasser.garcia@ooqia.com>
2018-09-28 13:16:37 -0600

    Initial checkin

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "jquery": true
+    },
+    "rules": {
+        "linebreak-style": [0, "error", "windows"]
+    }
+};
